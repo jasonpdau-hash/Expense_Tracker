b603677c07ee43e0a97a9cac8178e7ab0e337924
Jason UTS <jason.p.dau@student.uts.edu.au>
2026-05-27 21:38:38 +1000

    Update app to enable route to admin action page. Includes role authentication.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,28 @@ import Expense from './expense';
 import Login from './login';
 import Action from './action';
 
-// Route to user's page
+
+// Route to the login page
+const LoginRoute = ({ children }) => {
+  const user = localStorage.getItem('email');
+
+  // If there is no "email" item in local storage, then we can assume the user has not logged in yet
+  // So we return the login page
+  if (!user) {
+    return children;
+  }
+
+  // If we reach here, we check the priviliege level of the user.
+  // Route to action if admin, otherwise route to expense.  
+  const priviliege = localStorage.getItem('access_role');
+  if (priviliege == "admin") {
+    return <Navigate to="/action" replace />
+  } else {
+    return <Navigate to="/expense" replace />
+  }
+}
+
+// Route to expenses's page. Likewise, if there is no user we route to the login page.
 const UserRoute = ({ children }) => {
   const user = localStorage.getItem('email');
   if (!user) {
@@ -13,16 +34,19 @@ const UserRoute = ({ children }) => {
   }
 }
 
-// Route to login page
-const LoginRoute = ({ children }) => {
+// Route to actions's page. In addition, we will check the priviliege level of that user.
+// Route to actions if admin, otherwise return to login - if it is a user, they will be routed to expense instead.
+const ActionRoute = ({ children }) => {
   const user = localStorage.getItem('email');
-  if (user) {
-    return <Navigate to="/user" replace />
-  } else {
+  const priviliege = localStorage.getItem('access_role');
+  if (user && priviliege == "admin") {
     return children;
+  } else {
+    return <Navigate to="/login" replace />
   }
 }
 
+// No valid route
 function NoMatch() {
   return (
     <div>
@@ -37,8 +61,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<UserRoute><Expense /></UserRoute>} />
         <Route path="/login" element={<LoginRoute><Login /></LoginRoute>} />
-        <Route path="/user" element={<UserRoute><Expense /></UserRoute>} />
-        <Route path="/action" element={<UserRoute><Action /></UserRoute>} />
+        <Route path="/expense" element={<UserRoute><Expense /></UserRoute>} />
+        <Route path="/action" element={<ActionRoute><Action /></ActionRoute>} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </BrowserRouter>
